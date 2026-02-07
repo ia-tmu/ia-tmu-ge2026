fb0c46a3a2639963124c2c41cfdc3564d52698ba
menma275 <ohsusie9@gmail.com>
2026-02-07 19:04:43 +0900

    tagsのベクトル化、関数の作成

--- app/[locale]/works/page.tsx
+++ app/[locale]/works/page.tsx
@@ -1,15 +1,24 @@
 "use client"
 
+import { embeddingTags } from "../lib/tags"
+
 import { useParams } from "next/navigation";
 import Button from "../components/Button"
 import Section from "../components/Section"
 import { useTranslation } from "react-i18next"
 import { localePath } from "../lib/localePath";
+import sortTagsBySimilarity from "@/scripts/recommendTags";
+import { useEffect } from "react";
 
 export default function Works() {
   const params = useParams();
   const locale = params.locale;
   const { t } = useTranslation()
+  const list = sortTagsBySimilarity(["エディトリアルデザイン"])
+  console.log(list)
+  useEffect(() => {
+    console.log(list)
+  }, [list])
   return (
     <Section title={t("works.title")} subtitle={t("works.subtitle")}>
       <Button href={localePath(locale as string, "/")}>{t("works.back")}</Button>
